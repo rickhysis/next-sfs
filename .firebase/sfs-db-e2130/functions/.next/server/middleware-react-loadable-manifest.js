849b1729a6646280e73e2a710dff71849dfa14dc
rickhysis <rickhysiswanto@gmail.com>
2023-09-11 22:18:59 +0700

    fix firebase

--- .firebase/sfs-db-e2130/functions/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST='{"../components/layouts/auth/components/Sidebar.tsx -> @/components/layouts/auth/components/Menu":{"id":1444,"files":["static/chunks/664-0305aede6b1ca2d4.js","static/chunks/444.9460cb12d2c5855f.js"]}}';
